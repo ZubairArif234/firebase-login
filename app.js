321e7397ccbf1d9d2e87418a02be709b88421840
ZubairArif234 <104222183+ZubairArif234@users.noreply.github.com>
2022-10-02 08:45:44 -0700

    data base assignment

---
+++ app.js
@@ -0,0 +1,372 @@
+
+
+// // const signUpButton = document.getElementById("signUp");
+// // const signInButton = document.getElementById("signIn");
+// // const container = document.getElementById("container");
+
+// // signUpButton.addEventListener("click", () => {
+// //   container.classList.add("right-panel-active");
+// // });
+
+// // signInButton.addEventListener("click", () => {
+// //   container.classList.remove("right-panel-active");
+// // });
+
+
+
+
+
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+import { getAuth, 
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    sendSignInLinkToEmail
+ } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+ import { doc,
+   setDoc,
+   getDoc,
+   getFirestore,
+   getDocFromCache,
+   collection, 
+   getDocs,
+   query,
+    where
+ } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js"; 
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBKYcD_35A49akt0OitN62MQbMCwcKGvuU",
+  authDomain: "saylani-project-assignment.firebaseapp.com",
+  projectId: "saylani-project-assignment",
+  storageBucket: "saylani-project-assignment.appspot.com",
+  messagingSenderId: "677719458063",
+  appId: "1:677719458063:web:11e29676238dbb299cfe7a"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore()
+
+// // codepen end
+let register = document.getElementById("regiterBtn")
+
+
+var emailregix = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+var phoneregex = /^((\+92)?(0092)?(92)?(0)?)(3)([0-9]{9})$/;
+
+
+let registerEmail = document.getElementById("registerEmail");
+    let registerPassword = document.getElementById("registerPassword");
+    let registerName = document.getElementById("registerName");
+    let registerPhone = document.getElementById("registerPhone");
+    let registerQuali = document.getElementById("registerQuali");
+    let registerImage = document.getElementById("registerImg");
+
+
+// let register = document.getElementById("regiterBtn")
+
+register.addEventListener("click", function(event){
+  event.preventDefault()
+if (registerPassword.value.trim() === "" ){
+  Swal.fire(
+    'Error',
+    'Invalid Password (atleast 8 characters)',
+    'error'
+  )
+ }else if (registerName.value.trim() === "" ){
+  // alert("iok")
+  Swal.fire(
+    'Error',
+    'Invalid Name',
+    'error'
+  )
+ }else if (registerQuali.value.trim() === ""){
+  Swal.fire(
+    'Error',
+    'Invalid Qualification',
+    'error'
+  )
+ }else if (registerEmail.value === "" || !registerEmail.value.match(emailregix)){
+  Swal.fire(
+    'Error',
+    'Invalid Email',
+    'error'
+  )
+ }else if (registerPhone.value === "" || !registerPhone.value.match(phoneregex)){
+  Swal.fire(
+    'Error',
+    'Invalid phone number',
+    'error'
+  )
+ }
+ else{
+  createUserWithEmailAndPassword(auth, registerEmail.value, registerPassword.value)
+    
+    .then(async(userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log("USER",user)
+      await setDoc(doc(db, "users", user.uid), {
+        name: registerName.value,
+        email: registerEmail.value,
+        phone: registerPhone.value ,
+        qualification: registerQuali.value,
+        password: registerPassword.value,
+       
+
+
+      });
+      // ...
+      Swal.fire(
+        'Success',
+        'Successfully Registered ',
+        'success'
+      )
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      Swal.fire(
+        'Error',
+        'Email already in use',
+        'error'
+      )
+      // ..
+    });
+  
+ 
+ }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let arrfriend = [];
+
+
+
+
+
+// firebase login
+let login = document.getElementById("loginBtn")
+login.addEventListener("click", function(){
+
+    let loginEmail = document.getElementById("loginEmail");
+    let loginPassword = document.getElementById("loginPassword");
+
+    signInWithEmailAndPassword(auth, loginEmail.value, loginPassword.value)
+  .then(async (userCredential) => {
+    // Signed in 
+    
+    const user = userCredential.user;
+    console.log("USER",user)
+    Swal.fire(
+      'success',
+      `login successfully`,
+      'success'
+    )
+    // ...
+    const docRef = doc(db, "users", user.uid);
+const docSnap = await getDoc(docRef);
+
+
+
+
+window.localStorage.setItem("userobj", JSON.stringify(docSnap.data()))
+// window.location.href = "profile.html";
+if (docSnap.exists()) {
+  
+  console.log("Document data:", docSnap.data());
+  const querySnapshot = await getDocs(collection(db, "users")) ;
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, )//" => ", doc.data());
+    let  docsss =  doc.data()
+    console.log(docsss)
+     if( doc.id){
+    // let oi = document.getElementById("oi")
+    // console.log(oi)
+    // oi.innerHTML += `
+    // <table>
+  
+    // <tr ><td class="pup">${docsss.name}</td></tr>
+    // </table
+    // `
+    
+  }
+  arrfriend.push(docsss.name)
+    window.localStorage.setItem("namearr",JSON.stringify(arrfriend))
+    console.log(arrfriend)
+  });
+
+
+  
+  // window.localStorage.setItem("userobj", JSON.stringify(docSnap.data()))
+  
+  
+} else {
+  // doc.data() will be undefined in this case
+  console.log("No such document!");
+}
+
+window.location.href = "profile.html";
+
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message
+   let splitmessage = errorMessage.split("/")
+   
+  
+      Swal.fire(
+        'Error',
+        `(${splitmessage[1]}`,
+        'error'
+      )
+    // }
+    
+  });
+
+  
+  
+})
+
+
+
+
+
+
+
+
+// window.onload =()=>{
+//   // event.preventDefault()
+//     onAuthStateChanged(auth,async(user) => {
+//       // console.log(user)
+ 
+
+// const docRef = doc(db, "users", user.uid);
+// const docSnap = await getDoc(docRef);
+
+// if (docSnap.exists()) {
+  
+
+// let dta =  docSnap.data()
+//   let o = document.getElementById("o")
+//   o.innerHTML += `
+//   <table>
+//   <tr>
+//   <th class="pup">Name: </th><td class="pup">${dta.name}<td>
+//   </tr>
+//   <tr>
+//   <th class="pup">Email: </th><td class="pup">${dta.email}<td>
+//   </tr>
+//   <tr>
+//   <th class="pup">Nationality: </th><td class="pup">  ${dta.nationality}<td>
+//   </tr>
+//   <tr>
+//   <th class="pup">Age: </th><td class="pup">${dta.age}<td>
+//   </tr>
+//   <tr>
+//   <th class="pup">Phone: </th><td class="pup">${dta.number}<td>
+//   </tr>
+//   </table>
+//   `
+
+
+
+
+//   // import { collection, getDocs } from "firebase/firestore";
+
+//   const querySnapshot = await getDocs(collection(db, "users"));
+//   querySnapshot.forEach((doc) => {
+//     // doc.data() is never undefined for query doc snapshots
+//     console.log(doc.id, )//" => ", doc.data());
+//     let  docsss =  doc.data()
+//     console.log(docsss)
+//      if( doc.id){
+//     let oi = document.getElementById("oi")
+//     console.log(oi)
+//     oi.innerHTML += `
+//     <table>
+  
+//     <tr ><td class="pup">${docsss.name}</td></tr>
+//     </table
+//     `
+//   }
+//   });
+ 
+
+
+
+// } else {
+//   // doc.data() will be undefined in this case
+//   console.log("No such document!");
+// }
+
+
+//     if (!user) {
+//       const uid = user.Name;
+//       const uid2 = user.email;
+//       console.log( user )
+//       console.log(uid2)
+//       window.location = ("/index.html")
+//     } 
+     
+    
+//   });
+// }
+
+
+
+
+// //signout
+// var signout = document.getElementById("signout")
+// if(signout){
+// signout.addEventListener("click",()=>{
+//   const auth = getAuth();
+// signOut(auth).then(() => {
+//   // Sign-out successful.
+//   window.location = "/index.html"
+//   console.log("Successfull")
+// }).catch((error) => {
+//   // An error happened.
+// });
+// })} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app1.js
@@ -0,0 +1,208 @@
+
+
+
+
+
+
+
+
+
+
+
+
+// window.onload = () =>{
+let userdata = JSON.parse(window.localStorage.getItem("userobj"));
+console.log(userdata)
+
+
+
+let profileLIST = document.getElementById("itmlist");
+let profileLi =
+ `<li><b>${userdata.name}</b></li>
+<li>Email  =  ${userdata.email}</li>
+<li>Phone  =  ${userdata.phone}</li>
+<li>Qualification  =  ${userdata.qualification}</li>
+`
+profileLIST.innerHTML = profileLi;
+
+
+
+let frndarr = JSON.parse(window.localStorage.getItem("namearr"))
+console.log(frndarr);
+
+let yourfrnd = []
+console.log(yourfrnd)
+// let push = ()=> yourfrnd.push(frndarr[i])
+// document.getElementById("addbtn").addEventListener("click",push)
+
+let uefrndlist = document.getElementById("uefrndlist");
+let findlistul = document.getElementById("findlist");
+// for (var i = 0 ; i<frndarr.length ; i++ ){
+//  let findli = `<tr class="findli" id="qwert"><li  class="findli" id="qwert"><td>${frndarr[i]}</td><td><button class="btn btn-primary" id = "addbtn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Start Chat</button></td> </li></tr>`
+
+
+// findlistul.innerHTML += findli
+
+// }
+
+// firestore chat app
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+import { getAuth, 
+    onAuthStateChanged,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    sendSignInLinkToEmail
+ } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+ import {
+    doc,
+    setDoc,
+    getFirestore,
+    getDoc,
+    collection,
+    query,
+    where,
+    getDocs,
+    addDoc,
+    onSnapshot,
+    orderBy,
+  } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBKYcD_35A49akt0OitN62MQbMCwcKGvuU",
+    authDomain: "saylani-project-assignment.firebaseapp.com",
+    projectId: "saylani-project-assignment",
+    storageBucket: "saylani-project-assignment.appspot.com",
+    messagingSenderId: "677719458063",
+    appId: "1:677719458063:web:11e29676238dbb299cfe7a"
+  };
+
+  
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth();
+  const db = getFirestore()
+
+
+  window.onload = async () => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        if (!user.emailVerified) {
+          // sendEmailVerification(auth.currentUser)
+          //   .then(() => {
+          //     console.log("Email sent");
+          //   })
+          //   .catch((err) => console.log(err));
+        }
+        getUserFromDataBase(user.uid);
+      } else {
+        console.log("not login");
+      }
+    });
+  };
+
+const getUserFromDataBase = async (uid) => {
+    const docRef = doc(db, "users", uid);
+    // console.log(docRef)
+    const docSnap = await getDoc(docRef);
+    // let currentUser = document.getElementById("current-user");
+    if (docSnap.exists()) {
+    //   currentUser.innerHTML = `${docSnap.data().name} (${docSnap.data().email})`;
+      console.log("name= ", docSnap.data().name ,"id = ",docSnap.data().email )
+    getAllUsers(docSnap.data().email, uid, docSnap.data().name);
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  };
+// getUserFromDataBase()
+  
+  const getAllUsers = async (email, currentId, currentName) => {
+    const q = query(collection(db, "users"), where("email", "!=", email));
+    const querySnapshot = await getDocs(q);
+    let users = document.getElementById("users");
+    querySnapshot.forEach((doc) => {
+        // <tr class="findli" id="qwert"><li  class="findli" id="qwert"><td>${doc.data().name}</td><td><button onclick='startChat("${
+        //     doc.id
+        //   }","${
+        //     doc.data().name
+        //   }","${currentId}","${currentName}")' id="chat-btn">Start Chat</button></td> </li></tr>
+        // <button  class="btn btn-primary" id = "addbtn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Start Chat</button>
+        findlistul.innerHTML += `<tr class="findli" id="qwert"><li  class="findli" id="qwert"><td>${doc.data().name}</td><td><button class="btn btn-primary" id = "addbtn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" onclick='startChat("${
+            doc.id
+          }","${
+            doc.data().name
+          }","${currentId}","${currentName}")' id="chat-btn">Start Chat</button></td> </li></tr>`
+    //      `<li>${doc.data().name} <button onclick='startChat("${
+    //     doc.id
+    //   }","${
+    //     doc.data().name
+    //   }","${currentId}","${currentName}")' id="chat-btn">Start Chat</button></li>`;
+    });
+  };
+  
+  let unsubscribe;
+  let startChat = (id, name, currentId, currentName) => {
+    if (unsubscribe){
+        unsubscribe();
+    }
+    let chatWith = document.getElementById("chat-with");
+    chatWith.innerHTML = name;
+    let send = document.getElementById("send");
+    let message = document.getElementById("message");
+    // console.log(currentId)
+    let chatID;
+    if (id < currentId) {
+      chatID = `${id}${currentId}`;
+    } else {
+      chatID = `${currentId}${id}`;
+    }
+    loadAllChats(chatID,currentId);
+    send.addEventListener("click", async () => {
+      let allMessages = document.getElementById("all-messages");
+      allMessages.innerHTML = "";
+      await addDoc(collection(db, "messages"), {
+        sender_name: currentName,
+        receiver_name: name,
+        sender_id: currentId,
+        receiver_id: id,
+        chat_id: chatID,
+        message: message.value,
+        
+        timestamp: new Date(),
+        
+      });
+    //   console.log(sender_id,sender_name,receiver_id,receiver_name,message)
+    });
+    // console.log(doc.data().message)
+  };
+  
+  const loadAllChats = (chatID, currentId) => {
+    try {
+      const q = query(
+        collection(db, "messages"),
+        where("chat_id", "==", chatID),
+        // orderBy("timestamp", "asc")
+      );
+      let allMessages = document.getElementById("all-messages");
+      unsubscribe = onSnapshot(q, (querySnapshot) => {
+        allMessages.innerHTML = "";
+        // let date = new Date;
+        // let time = date.getHours() + " : " + date.getMinutes();
+        querySnapshot.forEach((doc) => {
+          let className =
+            doc.data().sender_id === currentId ? "my-message" : "user-message";
+          allMessages.innerHTML += `<div><li class="${className}">
+          
+           ${doc.data().message}
+           `;
+          
+        });
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
+  window.startChat = startChat;
